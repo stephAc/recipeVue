e76dbde56fd2dca3a7c5bd74a15b80516dde7c5a
itorterat <irvin.torterat@gmail.com>
2019-05-20 15:21:43 +0200

    Added different import to use Bootstrap-vue in application

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Creation from '@/pages/Creation'
+import Login from '@/pages/Login'
 
 Vue.use(Router)
 
@@ -15,6 +16,11 @@ export default new Router({
             path: '/recipe/create',
             name: 'Creation',
             component: Creation
+        },
+        {
+            path: '/user/login',
+            name: 'Login',
+            component: Login
         }
     ]
 })
