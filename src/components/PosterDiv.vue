7f0c8c329843ed098f6d5810d3dd8715e46e36dc
Ac <stephane.achari@u-psud.fr>
2019-05-27 11:01:23 +0200

    new branch list

---
+++ src/components/PosterDiv.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="poster"></div>
+</template>
+
+<script>
+export default {
+    name: 'Poster',
+    data: function() {
+        return {}
+    },
+    methods: {},
+}
+</script>
+
+<style>
+.poster {
+    height: 250px;
+    width: 250px;
+    min-width: 250px;
+    margin-right: 10px;
+    background-color: aqua;
+}
+</style>
