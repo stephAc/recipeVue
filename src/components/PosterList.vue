f798c37a58803f834d1c6d7c48035fff8c6e3905
Stephane <stephane.achari@u-psud.fr>
2019-06-01 16:17:54 +0200

    suppression getElementById & fix bug multiple list

--- src/components/PosterList.vue
+++ src/components/PosterList.vue
@@ -1,11 +1,10 @@
 <template>
   <div
+    ref="list"
     class="listDisplay"
     @mousedown="mousedown($event)"
     @mousemove="mousemove($event)"
     @mouseup="mouseup"
-    @wheel="mousewheel($event)"
-    id="listElement"
   >
     <PosterDiv v-for="n in 20" :key="n" :number="n"/>
   </div>
@@ -13,10 +12,7 @@
 
 <script>
 import PosterDiv from './PosterDiv'
-
 export default {
-    //     v-on:scroll="scroll"
-
     name: 'PosterList',
     data: function() {
         return {
@@ -29,38 +25,25 @@ export default {
     },
     methods: {
         mousedown: event => {
-            const listElement = document.getElementById('listElement')
+            const target = event.currentTarget
             this.mouseIsDown = true
             this.mouseIsUp = false
-
-            this.mouseBeginPositionX = event.pageX - listElement.offsetLeft
-            this.scrollLeft = listElement.scrollLeft
-
-            console.log(
-                'mouseIsDown : ',
-                this.mouseIsDown,
-                'mouseIsUp : ',
-                this.mouseIsUp
-            )
+            this.mouseBeginPositionX = event.pageX - target.offsetLeft
+            this.scrollLeft = target.scrollLeft
         },
         mousemove: event => {
-            const listElement = document.getElementById('listElement')
+            const target = event.currentTarget
             event.preventDefault()
 
             if (this.mouseIsDown) {
-                const x = event.pageX - listElement.offsetLeft
+                const x = event.pageX - target.offsetLeft
                 const scrollLength = (x - this.mouseBeginPositionX) * 3
-                listElement.scrollLeft = this.scrollLeft - scrollLength
+                target.scrollLeft = this.scrollLeft - scrollLength
             }
         },
         mouseup: () => {
             this.mouseIsUp = true
             this.mouseIsDown = false
-        },
-        mouseweel: event => {
-            const listElement = document.getElementById('listElement')
-            console.log('scroll')
-            listElement.scrollLeft -= 30 * 30
         },
     },
     components: {
